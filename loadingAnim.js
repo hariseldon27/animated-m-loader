78d468ca8cb642e73662c8e8809a1c84e133baed
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-27 23:05:00 -0800

    noodling around

---
+++ loadingAnim.js
@@ -0,0 +1,5 @@
+let animateButton = document.getElementById('animate-button')
+animateButton.addEventListener('click', function () {
+    let svganim = document.getElementById("mdot");
+	svganim.pauseAnimations();
+})
