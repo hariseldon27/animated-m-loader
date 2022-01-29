0bcafe4446c659b2d80e3209049ca57a7c049eb2
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-28 21:59:16 -0800

    did stuff

--- loadingAnim.js
+++ loadingAnim.js
@@ -4,18 +4,30 @@
 // 	svganim.pauseAnimations();
 // })
 
-const button = window.getElemenatbyId('animate-button')
+const button = document.getElementById('animate-button')
 const loadingDiv = document.getElementById('loading');
 
 button.addEventListener('click', function() {
   showSpinner();
-  
+  console.log("click")
 })
 
 function showSpinner() {
-  loadingDiv.style.display = "flex";
+  loadingDiv.style.display = "block";
+  insertAnimation()
 }
 
 function hideSpinner() {
   loadingDiv.style.display = "none";
 }
+
+function insertAnimation(){
+    loadingDiv.innerHTML =   
+
+}
+var svg = 'http://www.w3.org/2000/svg';
+        this.elt = document.createElementNS(svg,'line');
+        this.elt.setAttributeNS(null,'stroke','green');
+        this.elt.setAttributeNS(null,'stroke-width','3');
+        this.elt.setAttributeNS(null,'stroke-opacity','0.5');
+        Display().appendChild( this.elt );
