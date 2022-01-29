76b668375d411b43bf937e5afc895efc7a5e3d74
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-28 21:13:32 -0800

    making a loader

--- loadingAnim.js
+++ loadingAnim.js
@@ -1,5 +1,21 @@
-let animateButton = document.getElementById('animate-button')
-animateButton.addEventListener('click', function () {
-    let svganim = document.getElementById("mdot");
-	svganim.pauseAnimations();
+// let animateButton = document.getElementById('animate-button')
+// animateButton.addEventListener('click', function () {
+//     let svganim = document.getElementById("mdot");
+// 	svganim.pauseAnimations();
+// })
+
+const button = window.getElemenatbyId('animate-button')
+const loadingDiv = document.getElementById('loading');
+
+button.addEventListener('click', function() {
+  showSpinner();
+  
 })
+
+function showSpinner() {
+  loadingDiv.style.display = "flex";
+}
+
+function hideSpinner() {
+  loadingDiv.style.display = "none";
+}
